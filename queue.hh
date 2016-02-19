da8d6231dffddaaaed080138f4f6b828f8c009af
Omar Serrano <oser520@gmail.com>
2016-02-18 18:43:08 -0800

    Changed file name. Added header guards.

--- queue.hh
+++ queue.hh
@@ -1,9 +1,14 @@
 /**
- * @file pqueue.hh
+ * @file queue.hh
  * @author Omar A Serrano
  * @date 2016-01-15
  */
 
+#ifndef _QUEUE_H
+#define _QUEUE_H
+
 namespace ospp {
   // TODO: Create interface for priority queue.
 } // namespace ospp
+
+#endif /* _QUEUE_H */
