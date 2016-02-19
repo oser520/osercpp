44953c6bec9c1ab10785c2bc252ec439bfad09d9
Omar Serrano <oser520@gmail.com>
2016-02-18 18:59:52 -0800

    Created declaration of PriorityQueue. Created standard container aliases.

--- queue.hh
+++ queue.hh
@@ -8,7 +8,33 @@
 #define _QUEUE_H
 
 namespace ospp {
-  // TODO: Create interface for priority queue.
+
+// forward declaration
+template<typename T> class PriorityQueueIter;
+
+/**
+ * PriorityQueue.
+ */
+template<typename T, typename Alloc = std::allocator<T>>
+class PriorityQueue
+{
+  friend template<typename T> class PriorityQueueIter;
+public:
+  // aliases
+  using value_type = T;
+  using allocator_type = Alloc;
+  using reference = Alloc::reference;
+  using const_reference = Alloc::const_reference;
+  using pointer = Alloc::pointer;
+  using const_pointer = Alloc::const_pointer;
+  using iterator = PriorityQueueIter<value_type>;
+  using const_iterator = const PriorityQueueIter<value_type>;
+  using difference_type = std::iterator_traits<iterator>::difference_type;
+  using size_type = size_t;
+  // TODO: create alias for reverse iterator
+private:
+};
+
 } // namespace ospp
 
 #endif /* _QUEUE_H */
