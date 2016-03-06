85cdb22d22aa7d110874f543d3e07c035adca489
Omar Serrano <oser520@gmail.com>
2016-03-06 15:19:19 -0800

    Created public interface for PriorityQueue.

--- queue.hh
+++ queue.hh
@@ -8,6 +8,9 @@
 
 #include <memory>
 #include <functional>
+#include <string>
+#include <iterator>
+#include <type_traits>
 
 namespace ospp {
 
@@ -25,9 +28,16 @@ template
 >
 class PriorityQueue
 {
-  friend template<typename T> class PriorityQueueIter;
+  /**
+   * Default size for priority queue and size multiplier when items don't fit
+   * in priority queue
+   */
+  enum { DEFAULT_SIZE = 8, SIZE_MULT = 2 };
+
 public:
-  // aliases
+  /**
+   * Aliases
+   */
   using value_type = T;
   using allocator_type = Alloc;
   using reference = Alloc::reference;
@@ -35,16 +45,71 @@ public:
   using pointer = Alloc::pointer;
   using const_pointer = Alloc::const_pointer;
   using iterator = PriorityQueueIter<value_type>;
-  using const_iterator = const PriorityQueueIter<value_type>;
+  // TODO: determine how to write const iterator version
+  //using const_iterator = const PriorityQueueIter<value_type>;
   using difference_type = std::iterator_traits<iterator>::difference_type;
   using size_type = size_t;
   using compare_type = Compare;
   // TODO: create alias for reverse iterator
+
+  /**
+   * Initialize
+   */
+  explicit PriorityQueue(const size_t size);
+  explicit PriorityQueue(const allocator_type& alloc);
+  explicit PriorityQueue(const compare_type& comp);
+  explicit PriorityQueue
+    (const size_t size = DEFAULT_SIZE,
+     const compare_type& comp = compare_type(),
+     const allocator_type& alloc = allocator_type());
+  template
+  <
+    typename InputIterator,
+    typename = std::enable_if<is_input_iter<InputIterator>::value>::type
+  >
+  PriorityQueue(InputIterator first, InputIterator last);
+
+  /**
+   * Copy Construct
+   */
+  PriorityQueue(const PriorityQueue& cont);
+  PriorityQueue(PriorityQueue&& cont);
+
+
+  /**
+   * Assignment
+   */
+  PriorityQueue& operator=(const PriorityQueue& cont);
+  PriorityQueue& operator=(PriorityQueue&& cont);
+
+  /**
+   * Destructor
+   */
+  ~PriorityQueue() noexcept;
+
+  /**
+   * priority queue functionality
+   */
+  bool empty() const noexcept;
+  size_type size() const noexcept;
+  value_type top() const noexcept;
+  void push(const value_type& value);
+  void push(value_type&& value);
+  template<typename... Args>
+  void emplace(Args&&... args);
+  void pop();
+
+  /**
+   * object functionality
+   */
+  std::string toString() const;
+  size_type hashCode() const noexcept;
+
 private:
   /**
    * Pointer to the array of values in the priority queue.
    */
-  pointer mValues;
+  pointer mPtr;
 
   /**
    * The allocator.
@@ -57,7 +122,7 @@ private:
   compare_type mCompare;
 
   /**
-   * The maximum number of elements that may held.
+   * The maximum number of elements that may be held.
    */
   int mSize;
 
@@ -65,6 +130,13 @@ private:
    * The number of items held by the priority queue.
    */
   int mItems;
+
+  /**
+   * friends
+   */
+  friend std::ostream& operator<<(std::ostream&, const PriorityQueue&);
+  friend bool operator==(const PriorityQueue&, const PriorityQueue&) noexcept;
+  friend template<typename T> class PriorityQueueIter;
 };
 
 } // namespace ospp
