4ebc369ba8a38bf98d400b110279609bbdb1c1b9
Omar Serrano <oser520@gmail.com>
2016-03-06 15:23:43 -0800

    Created file to implement logic for iterator traits.

---
+++ iter_traits.cc
@@ -0,0 +1,21 @@
+/**
+ * @file iter_traits.hh
+ * @author Omar A Serrano
+ * @date 2016-03-06
+ *
+ * @description Implement logic to be able to determine characteristics
+ *  of iterators.
+ */
+#ifndef _ITER_TRAITS_H
+#define _ITER_TRAITS_H
+
+#include <iterator>
+#include <type_traits>
+
+namespace ospp {
+
+
+} // namespace ospp
+
+#endif /* _ITER_TRAITS_H */
+
