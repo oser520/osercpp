9cf854d30918fdc0bdffe6df5477da37206d02d4
Omar Serrano <oser520@gmail.com>
2016-03-07 20:18:27 -0800

    Created is_input_iter.

--- iter_traits.hh
+++ iter_traits.hh
@@ -14,6 +14,16 @@
 
 namespace ospp {
 
+/**
+ * Determines at compile time if an iterator carries the input_iterator_tag.
+ */
+template<typename Iter>
+struct is_input_iter {
+  using category = std::iterator_traits<Iter>::iterator_category;
+  using true_or_false = std::is_same<category, std::input_iterator_tag>;
+  using value = std::integral_constant<bool, true_or_false::value>;
+};
+
 
 } // namespace ospp
 
