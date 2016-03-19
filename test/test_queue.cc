25ff19e5dc6f881d1d53054105e2d60e2fdb5684
Omar Serrano <oser520@gmail.com>
2016-03-18 21:47:00 -0700

    Created shell source file to unit test priority queue.

---
+++ test/test_queue.cc
@@ -0,0 +1,26 @@
+/**
+ * @file test_queue.cc
+ * @author Omar A Serrano
+ * @date 2016-03-18
+ */
+
+#include "gtest/gtest.h"
+#include "queue.hh"
+
+namespace {
+
+/**
+ * Test PriorityQueue
+ */
+TEST(PriorityQueue, Ctors)
+{
+  // TODO: create unit tests for ctors
+}
+
+} // anonymous namespace
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
