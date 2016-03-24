dbcb4286c2aff337e83007ce401021a76f5e8d48
Omar Serrano <oser520@gmail.com>
2016-03-23 19:54:32 -0700

    Created unit test section for top and pop.

--- test/test_queue.cc
+++ test/test_queue.cc
@@ -38,6 +38,26 @@ TEST(PriorityQueue2, pushTest)
   EXPECT_EQ(10, pq.size());
 }
 
+// Test top and pop
+TEST(PriorityQueue2, topAndPopTest)
+{
+  PriorityQueue<int> pq;
+  pq.push(7);
+  pq.push(7);
+  pq.push(9);
+  pq.push(8);
+  pq.push(4);
+  pq.push(5);
+  pq.push(6);
+  pq.push(2);
+  pq.push(2);
+  pq.push(1);
+
+  EXPECT_EQ(10, pq.size());
+  EXPECT_EQ(1, pq.top());
+
+}
+
 } // anonymous namespace
 
 int main(int argc, char *argv[])
