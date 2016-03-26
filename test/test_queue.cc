61dac891b07877f181380a81e517acd4b04e41a8
Omar Serrano <oser520@gmail.com>
2016-03-25 20:45:41 -0700

    Call leftChild or rightChild.

--- test/test_queue.cc
+++ test/test_queue.cc
@@ -107,11 +107,11 @@ TEST(PriorityQueue6, leftChild)
   pq.push(2);
   pq.push(1);
 
-  EXPECT_EQ(1, pq.parent(0));
-  EXPECT_EQ(3, pq.parent(1));
-  EXPECT_EQ(5, pq.parent(2));
-  EXPECT_EQ(7, pq.parent(3));
-  EXPECT_EQ(9, pq.parent(4));
+  EXPECT_EQ(1, pq.leftChild(0));
+  EXPECT_EQ(3, pq.leftChild(1));
+  EXPECT_EQ(5, pq.leftChild(2));
+  EXPECT_EQ(7, pq.leftChild(3));
+  EXPECT_EQ(9, pq.leftChild(4));
 }
 
 // Test rightChild
@@ -129,11 +129,11 @@ TEST(PriorityQueue7, rightChild)
   pq.push(2);
   pq.push(1);
 
-  EXPECT_EQ(2, pq.parent(0));
-  EXPECT_EQ(4, pq.parent(1));
-  EXPECT_EQ(6, pq.parent(2));
-  EXPECT_EQ(7, pq.parent(3));
-  EXPECT_EQ(8, pq.parent(4));
+  EXPECT_EQ(2, pq.leftChild(0));
+  EXPECT_EQ(4, pq.leftChild(1));
+  EXPECT_EQ(6, pq.leftChild(2));
+  EXPECT_EQ(7, pq.leftChild(3));
+  EXPECT_EQ(8, pq.leftChild(4));
 }
 
 // Test top and pop
