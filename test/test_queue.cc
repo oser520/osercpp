ff9ddb73d7230a602b630ed30fc4485452e977ec
Omar Serrano <oser520@gmail.com>
2016-03-25 19:55:44 -0700

    Created unit test stub for hashCode.

--- test/test_queue.cc
+++ test/test_queue.cc
@@ -128,6 +128,38 @@ DISABLED_TEST(PriorityQueue6, toString)
   EXPECT_EQ(4, pq.top());
 }
 
+// Test hashCode
+// TODO: implement test
+DISABLED_TEST(PriorityQueue6, hashCode)
+{
+  PriorityQueue<int> pq;
+  pq.push(7);
+  pq.push(7);
+  pq.push(9);
+  pq.push(8);
+  pq.push(4);
+  pq.push(5);
+  pq.push(6);
+  pq.push(2);
+  pq.push(2);
+  pq.push(1);
+
+  EXPECT_EQ(10, pq.size());
+  EXPECT_EQ(1, pq.top());
+
+  pq.pop();
+  EXPECT_EQ(9, pq.size());
+  EXPECT_EQ(2, pq.top());
+
+  pq.pop();
+  EXPECT_EQ(8, pq.size());
+  EXPECT_EQ(2, pq.top());
+
+  pq.pop();
+  EXPECT_EQ(7, pq.size());
+  EXPECT_EQ(4, pq.top());
+}
+
 } // anonymous namespace
 
 int main(int argc, char *argv[])
