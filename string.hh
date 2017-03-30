a9a15c9677cdeaea83eba842104125c8b919d8d6
Omar Serrano <oser520@gmail.com>
2017-03-29 21:49:53 -0700

    Define arePermutations()

--- string.hh
+++ string.hh
@@ -9,6 +9,7 @@
 #include <bitset>
 #include <limits>
 #include <string>
+#include <unordered_map>
 
 namespace ospp {
 
@@ -22,6 +23,33 @@ hasUniqueCharacters(const std::basic_string<TChar> &value)
       return false;
     charSet.set(value);
   }
+  return true;
+}
+
+template<typename TChar>
+bool
+arePermutations(
+  const std::basic_string<TChar> &lhs,
+  const std::basic_string<TChar> &rhs)
+{
+  if (lhs.size() != rhs.size())
+    return false;
+
+  std::unordered_map<TChar, int> charMap;
+  for (auto c : lhs)
+    ++charMap[c];
+
+  for (auto c : rhs) {
+    auto val = --charMap[c];
+    if (val < 0)
+      return false;
+  }
+
+  for (auto values : charMap) {
+    if (not values.second)
+      return false;
+  }
+
   return true;
 }
 
