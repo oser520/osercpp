c841f7aaf676f6c5bb1430d545950c763b7a440c
Omar Serrano <oser520@gmail.com>
2017-04-03 21:48:35 -0700

    Define removeDuplicates()

--- linked-lists/snode.hh
+++ linked-lists/snode.hh
@@ -23,3 +23,23 @@ template<T>
 Node<T>::Node(T &data) noexcept(std::is_nothrow_copy_constructible<T>::value)
   : data{data}
 {}
+
+template<typename T>
+void
+removeDuplicates(Node<T> *node)
+{
+  std::set<T> found;
+  auto prev = node;
+  while (node) {
+    auto it = found.insert(node->data);
+    if (not it->second) {
+      auto tmp = node;
+      node = node->next;
+      prev->next = node;
+      delete tmp;
+      continue;
+    }
+    prev = node;
+    node = node->next;
+  }
+}
