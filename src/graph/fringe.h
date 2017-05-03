98577f9b64029c76071854ad280cda56bf95b6e6
Omar Serrano <oser520@gmail.com>
2017-05-02 21:44:37 -0700

    Define fringe for queue

--- src/graph/fringe.h
+++ src/graph/fringe.h
@@ -1,7 +1,9 @@
 #pragma once
 
 
+#include <algorithm>
 #include <type_traits>
+#include <utility>
 
 
 namespace ospp {
@@ -19,6 +21,72 @@ public:
   T next() const noexcept(std::is_nothrow_copy_constructible<T>::value);
   void pop() noexcept(std::is_nothrow_destructible<T>::value);
 };
+
+
+template<typename T>
+class Fringe<T, std::queue>
+{
+  std::queue<T> fringe;
+
+public:
+  bool empty() const noexcept;
+  bool contains(const T &t) const noexcept;
+  void push(const T &t);
+  void push(T &&t);
+  T next() const noexcept(std::is_nothrow_copy_constructible<T>::value);
+  void pop() noexcept(std::is_nothrow_destructible<T>::value);
+};
+
+
+template<typename T>
+bool
+Fringe<T, std::queue>::empty() const noexcept
+{
+  return fringe.empty();
+}
+
+
+template<typename T>
+bool
+Fringe<T, std::queue>::contains(const T &t) const noexcept
+{
+  auto last = fringe.cend();
+  return std::find(fringe.cbegin(), last, t) != last;
+}
+
+
+template<typename T>
+void
+Fringe<T, std::queue>::push(const T &t)
+{
+  fringe.push(t);
+}
+
+
+template<typename T>
+void
+Fringe<T, std::queue>::push(T &&t)
+{
+  fringe.push(std::move(t));
+}
+
+
+template<typename T>
+T
+Fringe<T, std::queue>::next() const
+noexcept(std::is_nothrow_copy_constructible<T>::value)
+{
+  return fringe.front();
+}
+
+
+template<typename T>
+void
+Fringe<T, std::queue>::pop() const
+noexcept(std::is_nothrow_destructible<T>::value)
+{
+  fringe.pop();
+}
 
 
 } // namespace ospp
