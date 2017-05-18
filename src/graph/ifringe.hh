988912a5e38a7c1e310ca9bf09b356069a20adda
Omar Serrano <oser520@gmail.com>
2017-05-17 21:25:45 -0700

    Declare as virtual and add whitespace

--- src/graph/ifringe.hh
+++ src/graph/ifringe.hh
@@ -11,12 +11,23 @@ template<typename T>
 class IFringe
 {
 public:
-  bool empty() const noexcept;
-  bool contains(const T &t) const noexcept;
-  void push(const T &t);
-  void push(T &&t);
-  T next() const noexcept(std::is_nothrow_copy_constructible<T>::value);
-  void pop() noexcept(std::is_nothrow_destructible<T>::value);
+  virtual bool
+  empty() const noexcept;
+
+  virtual bool
+  contains(const T &t) const noexcept;
+
+  virtual void
+  push(const T &t);
+
+  virtual void
+  push(T &&t);
+
+  virtual T
+  next() const noexcept(std::is_nothrow_copy_constructible<T>::value);
+
+  virtual void
+  pop() noexcept(std::is_nothrow_destructible<T>::value);
 };
 
 
