c419b3d25cb06ee2af2dadf6e49feba8335de62b
Omar Serrano <oser520@gmail.com>
2017-05-26 21:46:51 -0700

    Create unit tests for FifoFringe

---
+++ test/test_fifo_fringe.cc
@@ -0,0 +1,44 @@
+/**
+ * @file test_fifo_fringe.cc
+ * @author Omar A Serrano
+ * @date 2017-05-26
+ */
+
+#include "gtest/gtest.h"
+#include "graph/fifo_fringe.hh"
+
+
+using namespace ospp;
+
+
+namespace {
+
+
+struct TestFringe : ::testing::Test
+{
+  FifoFringe<int> intFringe;
+};
+
+
+TEST_F(TestFifoFringe, EmptyShouldReturnTrueIfFringeIsEmpty)
+{
+  EXPECT_TRUE(intFringe.empty());
+}
+
+
+TEST_F(TestFringe, PushingToFringeShouldResultInNonEmptyFringe)
+{
+  intFringe.push(1);
+  EXPECT_FALSE(intFringe.empty());
+}
+
+
+TEST_F(TestFringe, NextShouldReturnTheNextItem)
+{
+  intFringe.push(1);
+  intFringe.push(2);
+  EXPECT_EQ(1, intFringe.next());
+}
+
+
+} // anonymous namespace
